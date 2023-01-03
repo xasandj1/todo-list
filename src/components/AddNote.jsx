f40fbb5e2f71e1676d6f7980e094d53d7c138ac9
xasan <xasan>
2023-01-02 23:37:04 -0800

    section

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -5,26 +5,45 @@ import Button from "./Ul/Button"
 import {GoPencil} from "react-icons/go"
 const AddNote = () => {
     const [ active, setActive] = useState(false)
+    const {leng} = useContext(Context)
+    const [title , setTitle] = useState("")
+    const [text,setText] = useState("")
+    const titleChange = (e)=>{
+        setTitle(e.target.value) 
+    }
+    const textChange = (e)=>{
+        setText (e.target.value)     
+    }
     const addActive = ()=>{
         setActive(!active)
     }
-    const {leng} = useContext(Context)
+    const noteSubmit = (e) =>{
+        e.preventDefault()
+        if (title.trim() !== "" || text.trim() !== "") {
+            const newNote = {
+                id:new Date().getTime().toString(),
+                title:title,
+                text:text,
+                time:new Date().toLocaleDateString()
+            }
+        }
+    }
   return (
     <>
     <div className={`add-note ${active ? "active" : ''}`}>
         <Card classes={"card"}>
-            <form>
+            <form onSubmit={noteSubmit}>
                 <h2 className='title'>{leng.addNote}</h2>
                 <label className='add-label'>
                     {leng.inputTitle}
-                    <input type="text" placeholder={leng.inputTitle} className="add-input"/>                   
+                    <input type="text" placeholder={leng.inputTitle} className="add-input" value={title} onChange={titleChange}/>                   
                 </label>
                 <label className='add-label'>
                     {leng.inputText}
-                    <input type="text" placeholder={leng.inputTitle} className="add-input"/>
+                    <input type="text" placeholder={leng.inputTitle} className="add-input" value={text} onChange={textChange}/>
                 </label>
                 <div className='note-btn'> 
-                    <Button classes={"btn-danger"}>
+                    <Button classes={"btn-danger"} click={addActive}>
                         <span>{leng.close}</span>
                     </Button>
                     <Button classes={"btn-secondary"} type="submit">
